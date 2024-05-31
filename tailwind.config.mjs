300ce36518bf61d4442422f48fa75d42256f94dd
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-05-31 12:20:50 +0200

    some changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,27 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			'animation': {
+				'text-gradient': 'text-gradient 1.5s linear infinite',
+				'background-shine': 'background-shine 2s linear infinite'
+			},
+			'keyframes': {
+				'text-gradient': {
+					'to': {
+						'backgroundPosition': '200% center'
+					}
+				},
+				'background-shine': {
+					'from': {
+						'backgroundPosition': '0 0'
+					},
+					'to': {
+						'backgroundPosition': '-200% 0'
+					},
+				}
+			},
+		},
 	},
 	plugins: [],
 }
